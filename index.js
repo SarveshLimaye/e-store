fc5f03c78f985a1530f3aebe28dcd315a1ef8b4a
SarveshLimaye <sarveshlimaye2002@gmail.com>
2022-06-07 14:32:18 +0530

    connected with database :sparkles:

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
-require('dotenv').config()
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, './.env') });
 const express = require('express')
 const app = express()
 
 const notFoundMiddleware = require('./middleware/not-found')
 const errorHandlerMiddleware = require('./middleware/error-handler')
+const connectDB = require('./db/connect')
 
 //middleware
 app.use(express.json())
+app.use(notFoundMiddleware)
+app.use(errorHandlerMiddleware)
+
+const port = process.env.PORT || 3000
+
+const start = async () => {
+    try{
+       await connectDB(process.env.MONGODB_URI)
+      app.listen(port , console.log(`Server started at port ${port}`))
+    } catch(err) {
+        console.log(err)
+    }
+}
+
+start()
