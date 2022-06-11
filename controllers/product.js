1c10a04abe8b4dc6eea5f2b4f2517665b7a5a17d
SarveshLimaye <sarveshlimaye2002@gmail.com>
2022-06-11 18:55:08 +0530

    filter and search route done :sparkles:

--- controllers/product.js
+++ controllers/product.js
@@ -2,12 +2,20 @@ const Product = require('../models/product')
 const jsonProduct = require('../product.json')
 
 const getAllStaticProducts = async (req, res) => {
-   res.status(200).send(jsonProduct)
+   // res.status(200).send(jsonProduct)
    await Product.create(jsonProduct)
 }
+
+const searchProducts = async (req,res) => {
+
+   let searchData = await Product.find({"$or":[{name:req.query.name},{company:req.query.company}]})
+   res.status(200).send(searchData)
+}
+
+
 
 const getAllProducts = async (req, res) => {
    res.status(200).json({ msg:'products route' })
 }
 
-module.exports = {getAllProducts, getAllStaticProducts}
+module.exports = {getAllProducts, getAllStaticProducts ,searchProducts}
--- routes/products.js
+++ routes/products.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const router = express.Router()
 
-const {getAllProducts, getAllStaticProducts} = require('../controllers/product')
+const {getAllProducts, getAllStaticProducts , searchProducts} = require('../controllers/product')
 
 // console.log(getAllProducts)
 
 router.route('/').get(getAllProducts)
 router.route('/static').get(getAllStaticProducts)
+router.route('/search').get(searchProducts)
 
 module.exports = router
