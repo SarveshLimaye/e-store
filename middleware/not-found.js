25420e9fdbc2210b2f8a1be64472805134457216
SarveshLimaye <sarveshlimaye2002@gmail.com>
2022-06-14 19:18:01 +0530

    backend files in root directory deleted :sparkles:

--- middleware/not-found.js
+++
@@ -1,3 +0,0 @@
-const notFound = (req, res) => res.status(404).send('Page not found')
-
-module.exports = notFound
--- routes/user.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-
-const {registerUser , authUser} = require('../controllers/user')
-
-router.route('/').post(registerUser)
-router.route('/login').post(authUser)
-
-module.exports = router
