88328b95aedaf783c932e1116df09817cecdce84
SarveshLimaye <sarveshlimaye2002@gmail.com>
2022-07-17 10:20:05 +0530

    Search filter in frontend :sparkles:

--- frontend/src/components/Products.js
+++ frontend/src/components/Products.js
@@ -1,19 +1,24 @@
 import { useEffect , useState} from "react";
 import ProductCard from "./ProductCard";
 import { Grid } from "@mui/material";
-import Box from '@mui/material/Box';
+import Search from "./Search";
 
 const Products = () => {
     const[products,setProducts]=useState([])
+    const [query,setQuery]=useState("")
     useEffect(()=>{
      const fetchApi = async () => {
-        const response = await fetch(`http://localhost:5000/api/v1/products`)
+        const response = await fetch(query ? `http://localhost:5000/api/v1/products/search?name=${query}`: `http://localhost:5000/api/v1/products`);
         const resJson = await response.json()
         setProducts(resJson)
      }
      fetchApi()
-    },[])
+    },[query])
     return (
+        <div >
+        <div style={{display:'flex' , justifyContent:'center' , margin:'1rem 0 1rem 0'}}>
+         <Search getQuery={ (q) => {setQuery (q)}}/>
+         </div>
         <Grid container  spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 4, md: 12 }}>
       {products.map((product, index) => (
         <Grid item xs={12} sm={4} md={4} key={index}>
@@ -28,6 +33,7 @@ const Products = () => {
         </Grid>
       ))}
     </Grid>
+    </div>
     );
 }
 
---
+++ frontend/src/components/Search.js
@@ -0,0 +1,34 @@
+import Paper from '@mui/material/Paper';
+import InputBase from '@mui/material/InputBase';
+import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
+import { useState } from 'react';
+import SearchIcon from '@mui/icons-material/Search';
+
+const Search = ({getQuery}) => {
+    const [search, setSearch] = useState('');
+
+    const onChange = (q) => {
+        setSearch(q)
+        getQuery(q)
+    }
+    return (
+        <Paper
+        component="form"
+        sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: 400 }}
+      >
+        <InputBase
+          sx={{ ml: 1, flex: 1 }}
+          placeholder="Search Products"
+          inputProps={{ 'aria-label': 'search google maps' }}
+          onChange={(e) => { onChange(e.target.value) }}
+        />
+        <IconButton type="submit" sx={{ p: '10px' }} aria-label="search">
+          <SearchIcon style={{ fill: "blue" }}/>
+        </IconButton>
+        <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
+      </Paper>
+    )
+}
+
+export default Search;
