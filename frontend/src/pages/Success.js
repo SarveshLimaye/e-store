3a32e23614af2242baada7a906565d13875c8282
SarveshLimaye <sarveshlimaye2002@gmail.com>
2022-09-29 18:13:42 +0530

    updated ui of success page :sparkles:

--- frontend/src/pages/Success.js
+++ frontend/src/pages/Success.js
@@ -1,8 +1,31 @@
+import { Box, Typography ,Button} from "@mui/material"
+import Confetti from 'react-confetti'
+import { Link } from "react-router-dom"
+
 const Success = () => {
     return (
-        <div>
-        <h1>Order placed Successfully </h1>
-    </div>
+        <>
+        
+        <Box
+  display="flex"
+  justifyContent="center"
+  mt={6}
+>
+ 
+  <Typography variant="h5" color={"dark-green"}>Thank you for shopping with us !</Typography>
+ 
+</Box>
+
+<Box
+ justifyContent="center"
+ display="flex"
+  mt={6}
+>
+   <Link to="/" style={{'textDecoration':'none'}}><Button variant="contained"> Back to Home</Button></Link>
+</Box>
+
+<Confetti width={window.innerWidth} height={window.innerHeight}/>
+</>
     )
    
 
