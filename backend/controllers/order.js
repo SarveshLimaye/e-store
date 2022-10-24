bf71c8deb48eeb781029a0c843cc088212155bbf
SarveshLimaye <sarveshlimaye2002@gmail.com>
2022-10-24 13:45:21 +0530

    populate products in order module done :sparkles:

--- backend/controllers/order.js
+++ backend/controllers/order.js
@@ -1,19 +1,25 @@
 const Order = require('../models/order');
+const Product = require('../models/product');
 const asyncHandler = require('express-async-handler')
 
 const getAllOrders = async (req,res) => {
     res.status(200).json(await Order.find())
 }
 
-const getOrderbyId = asyncHandler(async (req,res) => {
-    const order = await Order.find({userId:req.params.id})
-    if(order){
-        res.status(200).json(order)
-    } else {
-        res.status(404)
-        throw new Error('Order not found')
-    }
-})
+const getOrderbyId = async (req,res) => {
+    const order = await Order.findOne({userId:req.params.id},
+     (err,product) => {
+         if(err){
+            console.log(err)
+         }
+         product.populate("product").execPopulate(() => {
+            console.log(product)
+            res.status(200).json(product)
+         })
+     }        
+        
+)
+}
 
 module.exports = {
     getAllOrders,getOrderbyId
